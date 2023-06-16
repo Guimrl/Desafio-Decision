e81758cee49e28d1a78918071b6ac07bc121822e
Guilherme Amaral <gui.amaral123@outlook.com>
2023-06-16 18:15:50 +0000

    Segunda parte concluida

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-document.querySelector("main").innerHTML += "<p style='color: #fff'>Hello, world</p>";
+const nome = document.querySelector("#nome");
+const idade = document.querySelector("#idade");
+const input = document.querySelectorAll("input");
+const erroNome = document.querySelector("#erro-nome");
+const erroIdade = document.querySelector("#erro-idade");
+
+input.forEach(el => {
+    el.addEventListener("blur", (event) => {
+        event.preventDefault;
+        erroNome.innerHTML = '';
+        erroIdade.innerHTML = '';
+
+        if (nome.value.length > 10) {
+            console.log("Limite 10 caracteres");
+            erroNome.innerHTML = 'Limite 10 caracteres';
+        }
+
+        if (idade.value < 1 || idade.value > 9 || idade.value.length !== 1) {
+            console.log("Deve ser entre 0 e 10");
+            erroIdade.innerHTML = 'Deve ser entre 0 e 10';
+        }
+    });
+});
+
